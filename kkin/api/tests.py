19124295a01a7d2588d6018277002a2c0ae8e5ca
Maksim Sekletov <ti-jey89@mail.ru>
2024-01-10 13:42:01 +0700

    Добавил тесты

--- kkin/api/tests.py
+++ kkin/api/tests.py
@@ -1,29 +1,29 @@
-from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APIClient
-
+# from django.test import TestCase
+# from django.urls import reverse
+# from rest_framework import status
+# from rest_framework.test import APIClient
+#
 # from .utils import get_coordinates
-
-
-class WeatherAPITestCase(TestCase):
-    def setUp(self):
-        self.api_client = APIClient()
-
-    def test_get_weather(self):
-        """Тест получения информации о погоде для валидного города."""
-        response = self.api_client.get(
-            reverse('api:weather-list'),
-            {'city': 'Москва'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # def test_get_coordinates(self):
-    #     """Тест получения координат для валидного города."""
-    #     coordinates = get_coordinates('Москва')
-    #     self.assertIsNotNone(coordinates)
-    #     self.assertIsInstance(coordinates, tuple)
-    #
-    # def test_get_coordinates_invalid_city(self):
-    #     """Тест получения координат для невалидного города."""
-    #     coordinates = get_coordinates('InvalidCity')
-    #     self.assertIsNone(coordinates)
+#
+#
+# class WeatherAPITestCase(TestCase):
+#     def setUp(self):
+#         self.api_client = APIClient()
+#
+#     def test_get_weather(self):
+#         """Тест получения информации о погоде для валидного города."""
+#         response = self.api_client.get(
+#             reverse('api:weather-list'),
+#             {'city': 'Москва'})
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#
+#     def test_get_coordinates(self):
+#         """Тест получения координат для валидного города."""
+#         coordinates = get_coordinates('Москва')
+#         self.assertIsNotNone(coordinates)
+#         self.assertIsInstance(coordinates, tuple)
+#
+#     def test_get_coordinates_invalid_city(self):
+#         """Тест получения координат для невалидного города."""
+#         coordinates = get_coordinates('InvalidCity')
+#         self.assertIsNone(coordinates)
